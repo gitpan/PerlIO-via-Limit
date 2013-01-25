8d82f1d1aa38c9c930ee874739274ec4f356104d
WATANABE Hiroaki <hwat@cpan.org>
2013-01-25 12:52:15 +0000

    Import of HWAT/PerlIO-via-Limit-0.03 from CPAN.
    gitpan-cpan-distribution: PerlIO-via-Limit
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         HWAT/PerlIO-via-Limit-0.03.tar.gz
    gitpan-cpan-author:       HWAT
    gitpan-cpan-maturity:     released

--- lib/PerlIO/via/Limit.pm
+++ lib/PerlIO/via/Limit.pm
@@ -3,26 +3,33 @@ package PerlIO::via::Limit;
 use strict;
 use warnings;
 use vars qw($VERSION);
-$VERSION = '0.02';
+$VERSION = '0.03';
+
+our @ISA;
+unshift @ISA, qw(Class::Data::Inheritable);
 
 use Exception::Class ('PerlIO::via::Limit::Exception');
 
-my $max_length  = undef;
-my $sensitive   = undef;
+__PACKAGE__->mk_classdata('length');
+__PACKAGE__->mk_classdata('sensitive');
 
 sub import {
     my ($class, %params) = @_;
     $class->$_( $params{$_} ) for keys %params;
 }
 
-sub length {
-    my $class = shift;
-    return @_ ? $max_length = shift : $max_length;
-}
+my $create_count = 0;
+sub create {
+    my ($class, $len, $new_class) = @_;
 
-sub sensitive {
-    my $class = shift;
-    return @_ ? $sensitive = shift : $sensitive;
+    $new_class = sprintf('%s::_%s', __PACKAGE__, ++$create_count)
+        unless( defined $new_class );
+
+    no strict 'refs';
+    @{"$new_class\::ISA"} = __PACKAGE__;
+
+    $new_class->length($len);
+    return $new_class;
 }
 
 sub PUSHED {
@@ -41,7 +48,6 @@ sub FILL {
         return undef;
     }
 
-    my $pos = tell $fh;
     my $buf = <$fh>;
 
     if( defined $buf ){
@@ -73,8 +79,8 @@ sub WRITE {
 
 sub _check {
     my ($obj, $ref_buf) = @_;
-    if( defined $max_length ){
-        my $over = $obj->{current} - $max_length;
+    if( defined(my $len = $obj->length) ){
+        my $over = $obj->{current} - $len;
         if( 0 <= $over ){
             $obj->{reached} = 1;
             substr($$ref_buf, $over * -1, $over, q{});
@@ -88,6 +94,7 @@ sub _check {
 1;
 __END__
 
+
 =head1 NAME
 
 PerlIO::via::Limit - PerlIO layer for length restrictions
@@ -95,9 +102,9 @@ PerlIO::via::Limit - PerlIO layer for length restrictions
 =head1 SYNOPSIS
 
     use PerlIO::via::Limit;
-    PerlIO::via::Limit->length(20);
+    PerlIO::via::Limit->length(256);
     # - or -
-    use PerlIO::via::Limit length => 20;
+    use PerlIO::via::Limit length => 256;
 
     # reading
     open( my $fh, "<:via(Limit)", $file );
@@ -105,15 +112,88 @@ PerlIO::via::Limit - PerlIO layer for length restrictions
     # writing
     open( my $fh, ">:via(Limit)", $file );
 
+    # If you want to use various limits simultaneously
+    my $limit256 = PerlIO::via::Limit->create(256);
+    my $limit512 = PerlIO::via::Limit->create(512);
+    open( my $fh256, "<:via($limit256)", $hoge );
+    open( my $fh512, ">:via($limit512)", $fuga );
+
 =head1 DESCRIPTION
 
 PerlIO::via::Limit implements a PerlIO layer that restricts length of stream.
 
+There is an important constraint, 
+it is able to specify only one limit value within application 
+because the 'length' is a class data.
+
+The following example does not work as expected:
+
+    PerlIO::via::Limit->length(256);
+    open( my $fh1, "<:via(Limit)", $file1 );
+
+    PerlIO::via::Limit->length(512);
+    open( my $fh2, "<:via(Limit)", $file2 );
+
+    local $/ = undef;
+    my $data1 = <$fh1>; 
+    my $data2 = <$fh2>; 
+
+    CORE::length($data1); # is not 256 but 512
+    CORE::length($data2); # is also 512
+
+Therefore, it is necessary to divide namespace,
+in order to use two or more limit values simultaneously.
+
+    package Foo;
+    use base PerlIO::via::Limit;
+    
+    package main;
+    PerlIO::via::Limit->length(256);
+    Foo->length(512);
+
+    open( my $fh1, "<:via(Limit)", $file1 );
+    open( my $fh2, "<:via(Foo)", $file2 );
+
+    local $/ = undef;
+    my $data1 = <$fh1>; 
+    my $data2 = <$fh2>; 
+
+    CORE::length($data1); # is 256
+    CORE::length($data2); # is 512
+
+Actually you do not have to code like the above,
+instead, the create() method supports it by simple interface.
+
+    my $limit256 = PerlIO::via::Limit->create(256);
+    my $limit512 = PerlIO::via::Limit->create(512);
+
+    open( my $fh1, "<:via($limit256)", $file1 );
+    open( my $fh2, "<:via($limit100)", $file2 );
+
 =head1 CLASS METHODS
+
+=head2 create
+
+Create an anonymous class that is inheritable L<PerlIO::via::Limit>.
+
+You do not have to care about the class, only pass ':via' the returned value as it is.
+
+It accepts an optional parameter for 'length' available.
+    
+    my $limit = PerlIO::via::Limit->create(512);
+    open( my $fh, ">:via($limit)", $file );
+
+Also it can call 'length' and 'sensitive' class methods.
+
+    my $limit = PerlIO::via::Limit->create;
+    $limit->length(256);
+    $limit->sensitive(0);
+    open( my $fh, ">:via($limit)", $file );
 
 =head2 length
 
-Limit length of stream. Default is undef that means unlimited.
+Limit length of stream.
+Default is undef that means unlimited.
 
 =head2 sensitive
 
@@ -133,12 +213,16 @@ Default is false.
     }
     close $in or die;
 
-Note that the $@ is a Exception::Class object.
+Note that the $@ is an Exception::Class object.
 
 =head1 BUGS
 
 When the exception is thrown by sensitive option,
 the buffer for reading does not be filled.
+
+=head1 REPOSITORY
+
+PerlIO::via::Limit is hosted on github L<https://github.com/hiroaki/PerlIO-via-Limit>
 
 =head1 SEE ALSO
 
@@ -146,13 +230,11 @@ L<PerlIO::via>
 
 L<Exception::Class>
 
-=head1 REPOSITORY
-
-PerlIO::via::Limit is hosted on github L<https://github.com/hiroaki/PerlIO-via-Limit>
-
 =head1 AUTHOR
 
 WATANABE Hiroaki E<lt>hwat@cpan.orgE<gt>
+
+=head1 LICENSE
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
